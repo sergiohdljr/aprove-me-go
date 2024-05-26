a71287c345c3ac210b0e1668eaba1fabe7ec1130
sergiohdljr <sergio.tjf1@gmail.com>
2024-05-26 15:01:39 -0300

    fix: return r outside groupRoute

--- pkg/routes/route.go
+++ pkg/routes/route.go
@@ -1,30 +1,11 @@
 package route
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/sergiohdljr/aprove-me-go/pkg/handles"
 )
 
-type PaymentRequest struct {
-	ID              string          `json:"id"`
-	Value           float64         `json:"value"`
-	EmissionDate    string          `json:"emissionDate"`
-	Assignor        string          `json:"assignor"`
-	AssignorDetails AssignorDetails `json:"assignorDetails"`
-}
-
-type AssignorDetails struct {
-	ID       string `json:"id"`
-	Document string `json:"document"`
-	Email    string `json:"email"`
-	Phone    string `json:"phone"`
-	Name     string `json:"name"`
-}
-
 func RouteInnit() *gin.Engine {
-	// var db = new(map[string]string)
-
 	r := gin.Default()
 
 	r.GET("/ping", func(ctx *gin.Context) {
@@ -33,18 +14,10 @@ func RouteInnit() *gin.Engine {
 		})
 	})
 
-	r.POST("/integrations/payable", func(ctx *gin.Context) {
-		var body PaymentRequest
-
-		if err := ctx.ShouldBindJSON(&body); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(200, gin.H{
-			"body": body,
-		})
-	})
+	V1 := r.Group("/api/v1")
+	{
+		V1.POST("/integrations/payable", handles.RegisterPayble)
+	}
 
 	return r
 }
