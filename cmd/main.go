a05de09802fb5d08525dad476357880758b59eb3
sergiohdljr <sergio.tjf1@gmail.com>
2024-05-26 18:08:57 -0300

    feat: add innitDB

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/sergiohdljr/aprove-me-go/pkg/database"
 	route "github.com/sergiohdljr/aprove-me-go/pkg/routes"
 )
 
 func main() {
 	r := route.RouteInnit()
+
+	database.InnitDB()
 
 	r.Run()
 }
