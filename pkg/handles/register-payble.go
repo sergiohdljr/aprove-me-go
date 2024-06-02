62f3185d2a7fc03a99fbbfc93401bf342faf15e2
sergiohdljr <sergio.tjf1@gmail.com>
2024-06-02 10:58:00 -0300

    fix: register a payble

--- pkg/handles/register-payble.go
+++ pkg/handles/register-payble.go
@@ -1,14 +1,17 @@
 package handles
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sergiohdljr/aprove-me-go/pkg/database"
 	handle_errors "github.com/sergiohdljr/aprove-me-go/pkg/handles/errors"
+	"github.com/sergiohdljr/aprove-me-go/pkg/models"
 )
 
 type PaybleRequest struct {
-	ID              string          `json:"id"  binding:"required,uuid"`
 	Value           float64         `json:"value" binding:"required" `
 	EmissionDate    string          `json:"emissionDate" binding:"required" `
 	Assignor        string          `json:"assignor" binding:"required" `
@@ -16,7 +19,6 @@ type PaybleRequest struct {
 }
 
 type AssignorDetails struct {
-	ID       string `json:"id" binding:"required"`
 	Document string `json:"document" binding:"required,min=11,max=30"`
 	Email    string `json:"email" binding:"required,email,max=140"`
 	Phone    string `json:"phone" binding:"required,max=20"`
@@ -31,6 +33,29 @@ func RegisterPayble(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, handle_errors.ValidationError(err))
 		return
 	}
+
+	assignor := models.Assignor{
+		Document: body.AssignorDetails.Document,
+		Email:    body.AssignorDetails.Email,
+		Phone:    body.AssignorDetails.Phone,
+		Name:     body.AssignorDetails.Name,
+	}
+
+	if err := database.Db.Create(&assignor).Error; err != nil {
+		log.Fatalf("failed to create assignor: %v", err)
+	}
+
+	payment := models.Payment{
+		Value:        body.Value,
+		EmissionDate: time.Now(),
+		AssignorID:   assignor.ID,
+	}
+
+	if err := database.Db.Create(&payment).Error; err != nil {
+		log.Fatalf("failed to create payment: %v", err)
+	}
+
+	log.Printf("Payment created successfully with ID: %s", payment.ID)
 
 	ctx.JSON(http.StatusOK, body)
 }
