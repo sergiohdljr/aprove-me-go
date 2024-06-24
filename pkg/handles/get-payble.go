f1f5b27635888d009b631e3602ea3299b36da9c2
sergiohdljr <sergio.tjf1@gmail.com>
2024-06-24 20:59:46 -0300

    feat: tratando not found error

--- pkg/handles/get-payble.go
+++ pkg/handles/get-payble.go
@@ -1,6 +1,7 @@
 package handles
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/sergiohdljr/aprove-me-go/pkg/database"
 	"github.com/sergiohdljr/aprove-me-go/pkg/models"
@@ -9,6 +10,14 @@ import (
 
 func GetPayble(ctx *gin.Context) {
 	var payble models.Payment
-	database.Db.First(&payble, "id = ?", ctx.Param("id"))
+	payble_id := ctx.Param("id")
+
+	result := database.Db.First(&payble, "id = ?", payble_id)
+
+	if result.Error != nil {
+		ctx.Error(errors.New("Payble not found")).SetType(gin.ErrorTypePublic)
+		return
+	}
+
 	ctx.JSON(http.StatusOK, payble)
 }
